0892a8fc1ce422871e03973ee99c9d5850c4a50f
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-03-07 11:41:02 +0700

    feat: create 20240307025157-create-presidental-candidate.js

---
+++ db/migrations/20240307025157-create-presidental-candidate.js
@@ -0,0 +1,42 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Presidental_candidates", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      political_party_id: {
+        type: Sequelize.STRING,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      dob: {
+        type: Sequelize.STRING,
+      },
+      img_url: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Presidental_candidates");
+  },
+};
