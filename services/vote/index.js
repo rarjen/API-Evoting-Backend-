0548b08cf63fb15a0ef946daa6e2aa96f8229db1
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-03-08 11:49:25 +0700

    feat: create services/vote/index.js

---
+++ services/vote/index.js
@@ -0,0 +1,56 @@
+const { Voting_result, Voting_log, User, sequelize } = require("../../models");
+const crypto = require("crypto");
+const { encrypt, decrypt } = require("../../helpers/encryption");
+const ApiError = require("../../helpers/errorHandler");
+
+const create = async (req) => {
+  const user = req.user;
+  const transaction = await sequelize.transaction();
+  try {
+    const checkIsVoted = await User.findOne({
+      where: { id: user.id },
+      transaction,
+    });
+
+    if (checkIsVoted.isVoted === 1) {
+      throw ApiError.badRequest("Anda sudah melakukan vote!");
+    }
+
+    const result = await Voting_log.create(
+      {
+        encrypted_user_id: encrypt(user.id),
+        encrypted_candidate_number_id: encrypt(req.body.candidate_number_id),
+      },
+      { transaction }
+    );
+
+    await User.update({ isVoted: 1 }, { where: { id: user.id }, transaction });
+
+    await transaction.commit();
+    return result;
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
+};
+
+const getAll = async (req) => {
+  const result = await Voting_log.findAll({});
+
+  // const finalResult = result.map((data) => {
+  //   return {
+  //     user_id: decrypt(data.encrypted_user_id),
+  //     candidate_number_id: decrypt(data.encrypted_candidate_number_id),
+  //   };
+  // });
+
+  return result;
+};
+
+const getOne = async (req) => {
+  const result = await Voting_log.findOne({ where: { id: req.params.id } });
+
+  return result;
+};
+
+module.exports = { create, getAll, getOne };
