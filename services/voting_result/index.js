d29a8556c96e4c9c73ff390fde2e49074148fca1
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-03-15 10:15:31 +0700

    update services/voting_result/index.js

--- services/voting_result/index.js
+++ services/voting_result/index.js
@@ -1,5 +1,10 @@
 const ApiError = require("../../helpers/errorHandler");
-const { Voting_result, Candidate_pair_number } = require("../../models");
+const {
+  Voting_result,
+  Candidate_pair_number,
+  Presidental_candidate,
+  Vice_presidental_candidate,
+} = require("../../models");
 
 const create = async (req) => {
   req.body.total_vote = 0;
@@ -19,9 +24,30 @@ const create = async (req) => {
 };
 
 const getAll = async (req) => {
-  const result = await Voting_result.findAll({});
+  const result = await Voting_result.findAll({
+    include: {
+      model: Candidate_pair_number,
+      as: "number",
+      include: [
+        { model: Presidental_candidate, as: "presidental_candidate" },
+        { model: Vice_presidental_candidate, as: "vice_presidental_candidate" },
+      ],
+    },
+    order: [["total_vote", "ASC"]],
+  });
 
-  return result;
+  const modifiedResults = result.map((result) => ({
+    id: result.id,
+    candidate_pair_number_id: result.candidate_pair_number_id,
+    total_vote: result.total_vote,
+    percentage: result.percentage,
+    number: result.number.number,
+    img_result: result.number.img_url,
+    presidental_name: result.number.presidental_candidate.name,
+    vice_presidental_name: result.number.vice_presidental_candidate.name,
+  }));
+
+  return modifiedResults;
 };
 
 const getOne = async (req) => {
