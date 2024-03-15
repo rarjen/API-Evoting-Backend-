328a90e1c5255deaf7bf3305f81f1c29e3a2630c
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-03-15 08:50:17 +0700

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const vicePresidental = require("./vice_presidental");
 const pair_number = require("./candidate_pair_number");
 const supporting_parties = require("./supporting_parties");
 const vote = require("./vote");
+const voting_result = require("./voting_result");
 
 router.get("/test", (req, res) => {
   return responseHandler.succes(res, "Test Route");
@@ -19,5 +20,6 @@ router.use("/vice-presidental", vicePresidental);
 router.use("/pair-number", pair_number);
 router.use("/supporting-parties", supporting_parties);
 router.use("/vote", vote);
+router.use("/voting-result", voting_result);
 
 module.exports = router;
