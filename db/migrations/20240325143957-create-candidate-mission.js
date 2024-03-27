60bc95c9f032f9524c43af0d5f58b0050547f330
Otniel Kevin Abiel <rarjen57@gmail.com>
2024-03-27 10:46:37 +0700

    feat: create 20240325143957-create-candidate-mission.js

---
+++ db/migrations/20240325143957-create-candidate-mission.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Candidate_missions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      candidate_id: {
+        type: Sequelize.STRING
+      },
+      mission: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Candidate_missions');
+  }
+};
